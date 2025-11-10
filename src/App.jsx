a07a5bf2385df6b62e5a23f5dae24213613c781a
LucaStellato <lucastellato435@gmail.com>
2025-11-10 13:48:27 +0100

    insert input type

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <>
       <form onSubmit={addArticles}>
-        <input type='text' value={newArticles} onChange={(e) => setNewTitle(e.target.value)} placeholder />
+        <input type='text' value={newArticles} />
         <button>invia</button>
       </form>
       <ul>
