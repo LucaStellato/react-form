200b38877e93b8485f5ec2f32e36e103727b11bc
LucaStellato <lucastellato435@gmail.com>
2025-11-10 15:14:02 +0100

    add style to page

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,25 @@ export default function App() {
 
   return (
     <>
-      <form onSubmit={addArticles}>
-        <input type='text' value={newArticles} onChange={(e) => setNewArticles(e.target.value)} />
-        <button type='submit'>invia</button>
-      </form>
-      <ul>
-        {articles.map((article) => (
-          <li key={article.id}>{article.title}</li>
-        ))}
-      </ul>
+
+      <div style={{ textAlign: 'center' }}>
+        <h1>REACT FORM</h1>
+        <div style={{ display: 'inline-flex', flexDirection: 'column', marginTop: '80px', backgroundColor: 'blue', padding: '3px' }}>
+          <form onSubmit={addArticles}>
+            <input type='text' value={newArticles} onChange={(e) => setNewArticles(e.target.value)} />
+            <button type='submit'>invia</button>
+          </form>
+        </div>
+        <div style={{ textAlign: 'center' }}></div>
+        <div style={{ marginTop: '20px', backgroundColor: 'blue', display: 'inline-flex', padding: '45px' }}>
+          <ul>
+            {articles.map((article) => (
+              <li key={article.id}>{article.title}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
 
     </>
 
