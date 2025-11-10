896b76f579b7bcc00cb1021a95d6b60295938a4a
LucaStellato <lucastellato435@gmail.com>
2025-11-10 12:32:06 +0100

    insert array

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+export default function App() {
+  const articles = [
+    { id: 1, title: 'Primo articolo' },
+    { id: 2, title: 'Secondo articolo' },
+    { id: 3, title: 'Terzo articolo' },
+  ]
+  return (
+    
+  )
+}
